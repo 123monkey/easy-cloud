59c77c58fdf882b1df7cf49f33e729d9d38705be
daiqi <466608943@qq.com>
2018-11-19 23:32:28 +0800

    完善监控组件

---
+++ easy-cloud-core-parent/hutool-starter/src/main/java/HutoolApplication.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author daiqi
+ * @create 2018-11-19 23:28
+ */
+@SpringBootApplication
+public class HutoolApplication {
+    public static void main(String[] args) {
+
+    }
+}
